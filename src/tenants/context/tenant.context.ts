a929f33fa354712b14265b5c8f55982b161a60ad
Collins Nwoko C <collinsnwoko05@gmail.com>
2025-05-14 11:06:44 +0100

    updated tenant context

--- src/tenants/context/tenant.context.ts
+++ src/tenants/context/tenant.context.ts
@@ -21,7 +21,9 @@ export class TenantContext {
 
   // New method that throws if missing
   requireTenantId(): string {
-    const tenantId = this.request.tenantId;
+    // const tenantId = this.request.tenantId;
+    const tenantId = this.request.tenantId ?? this.request.headers['tenantid'] as string | undefined;
+    // console.warn(this.request); // Debugging line
     if (!tenantId) {
       throw new Error('Tenant ID not found in request context');
     }
